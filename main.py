c74fc565cb45b55c64b4cf164c90bd51300b6f6f
povilas <povilasleka@gmail.com>
2022-06-25 21:17:18 +0300

    main.py:convert() gets input_path & output_path from req args

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from services.convert_service import ConvertService
 
 app = Flask(__name__)
 
-@app.route('/convert')
-def hello():
-    return ConvertService("input_path", "output_path").run()
+@app.route('/convert', methods=["GET"])
+def convert():
+    args = request.args
+    ConvertService(args.get("input_path"), args.get("output_path")).run()
+    return 'done!'
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = '8080')
