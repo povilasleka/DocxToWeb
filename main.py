020f33c5223d785e2fdbd7ec1fd2858929ee7b2a
povilas <povilasleka@gmail.com>
2022-06-26 16:06:20 +0300

    move downloaded files to tmp folder

--- main.py
+++ main.py
@@ -7,14 +7,20 @@ import os
 app = Flask(__name__)
 app.debug = True
 
+if not os.path.isdir('tmp'):
+    try:
+        os.mkdir('tmp')
+    except OSError as error:
+        print(error)
+
 @app.route('/convert', methods=["POST"])
 def convert():
     file_base64 = request.get_json()["file"]
 
-    with open('input.pdf', 'wb') as f:
+    with open('tmp/input.pdf', 'wb') as f:
         f.write(base64.b64decode(file_base64))
 
-    ConvertService(f"{os.getcwd()}/input.pdf", "download.zip").run()
+    ConvertService(f"{os.getcwd()}/tmp/input.pdf", "./tmp/download.zip").run()
 
     return 'done!'
 
